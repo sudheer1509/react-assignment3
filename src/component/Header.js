299b7bf6ad88043f87c3f92f632e09088f7e2df5
Sudheer1509 <sudheervelishala99@gmail.com>
2022-08-23 10:22:04 +0530

    search bar added

--- src/component/Header.js
+++ src/component/Header.js
@@ -7,42 +7,15 @@ class Header extends Component {
   }
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a className="navbar-brand" href="index.html">
-          MOVIES
-        </a>
-        <div className={classes.searchContainer}>
-          <input
-            type="text"
-            placeholder="Search for movie..."
-            className={classes.searchBox}
-            value={this.props.searchQuery}
-            onChange={(e) => this.props.handleSearch(e)}
-          />
-        </div>
-        {/* <div className="searchContainer">
-          <input
-            type="search"
-            placeholder="Search for Movie Title ... "
-            className="searchBox"
-            onChange={(e) => setSearchedMovies(e.target.value)}
-          />
-        </div>
-        {searchedMovies.length < 3 && (
-          <p className="error">Enter atleast 3 characters</p>
-        )}
-        <div className="moviesWrapper">
-          {searchedMovieList &&
-          searchedMovieList.length !== 0 &&
-          searchedMovies !== ""
-            ? searchedMovieList.map((movie, idx) => (
-                <MovieCard movie={movie} idx={idx} />
-              ))
-            : allMovies.map((movie, idx) => (
-                <MovieCard movie={movie} idx={idx} />
-              ))}
-        </div> */}
-      </nav>
+      <div className={classes.searchContainer}>
+        <input
+          type="text"
+          placeholder="Search for movie..."
+          className={classes.searchBox}
+          value={this.props.searchQuery}
+          onChange={(e) => this.props.handleSearch(e)}
+        />
+      </div>
     );
   }
 }
